11bb24fe767874a741da3fd8da58298c524ce664
Math314br <matheuspedrosodesouza314@gmail.com>
2025-02-05 13:15:23 -0300

    criação do tamanho da jenela do programa e bordas

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ class Applicação():
     def __init__(self):
      self.janela= janela
      self.tela()
+     self.frameTELA()
      janela.mainloop()
     def tela(self):
        #TITULO PRINCIPAL DO PROGRAMA
@@ -21,5 +22,11 @@ class Applicação():
        self.janela.maxsize(width=730, height=1180)
        #Escolha tamanho minimo da tela
        self.janela.minsize(width=600, height= 900)
+    #esse frame sera basicamente as bordas do programa apenas uma janela basica
+    def frameTELA(self):
+       self.frame1 = Frame (self.janela, bd= 3, bg="#C0C0C0", highlightthickness=6, highlightbackground="black")
+       self.frame1.place(relx=0.025, rely=0.025, relwidth=0.95, relheight=0.95)
+       
+       
 
 Applicação()
