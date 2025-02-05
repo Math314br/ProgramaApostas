91622e9c18fb30a3ba63671ecf8848d08b5e535e
Math314br <matheuspedrosodesouza314@gmail.com>
2025-02-05 15:48:42 -0300

    CRIADO OS BOTAO DE GREEN E RED, MAS PARTE DE APOSTA. TUDO BASICO AINDA APENAS UM ESBOÇO RAPIDO

--- src/main.py
+++ src/main.py
@@ -13,6 +13,11 @@ class Applicação():
      self.botao_banca()
      self.ativar_edicao()
      self.atualizar_banca()
+     self.aposta()
+     self.valor_aposta()
+     self.valor_odd()
+     self.green()
+     self.red()
      janela.mainloop()
     def tela(self):
        #TITULO PRINCIPAL DO PROGRAMA
@@ -31,29 +36,35 @@ class Applicação():
     def frameTELA(self):
        self.frame1 = Frame (self.janela, bd= 3, bg="#F57C00", highlightthickness=10, highlightbackground="#2F4F4F")
        self.frame1.place(relx=0.025, rely=0.025, relwidth=0.95, relheight=0.95)
+
     #cria uma label aonde sera exibida o valor da banca e atualizado.
+
     def _banca(self):
-       self.label_banca = Label(self.frame1, text=f"Banca: R$ {self.banca:.2f}",bg="#F57C00",fg="white",font=('arial', 35,'bold') )
+       self.label_banca = Label(self.frame1, text=f"Banca: R$ {self.banca:.2f}",bg="#F57C00",fg="white",font=('arial', 30,'bold') )
        self.label_banca.place(relx=0.25, rely=0.01)
+
     #criar botao editar banca, aonde ele ira editar saldo da banca.
+
     def botao_banca(self):
-       self.botao_editar = Button(self.frame1, text="Editar Banca", font=("Arial", 20), command=self.ativar_edicao)
-       self.botao_editar.place(relx=0.05, rely=0.15)
+       self.botao_editar = Button(self.frame1, text="Editar Banca", font=("Arial", 13), command=self.ativar_edicao)
+       self.botao_editar.place(relx=0.25, rely=0.01)
+
     # função do botao que ira ativar edição
+
     def ativar_edicao(self):
        #ao apertar no botao ele esconde o botao
        self.botao_editar.place_forget ()
 
        #entrada do valor da banca
 
-       self.valor_banca = Entry(self.frame1, font=('arial', 20))
-       self.valor_banca.place(relx=0.05, rely=0.15)
+       self.valor_banca = Entry(self.frame1, font=('arial', 30))
+       self.valor_banca.place(relx=0.25, rely=0.01)
         # o valor da banca atual
        self.valor_banca.insert(0, str(self.banca))
 
        # Botão para confirmar a alteração da banca
-       self.botao_confirmar = Button(self.frame1, text="Confirmar", font=("Arial", 14), command=self.atualizar_banca)
-       self.botao_confirmar.place(relx=0.05, rely=0.2)
+       self.botao_confirmar = Button(self.frame1, text="Confirmar", font=("Arial", 15), command=self.atualizar_banca)
+       self.botao_confirmar.place(relx=0.04, rely=0.01)
 
         #atualizar banca 
     def atualizar_banca(self):
@@ -73,12 +84,40 @@ class Applicação():
           self.botao_confirmar.place_forget()
 
           # ira exibir novamente o botao editar
-          self.botao_editar.place(relx=0.05, rely=0.15)
+          self.botao_editar.place(relx=0.04, rely=0.01)
 
         #se valor nao for numero ira exibir um erro
        except ValueError:
           erro_banca = Label(self.frame1, text="Escreva apneas numeros!!", font=('arial', 20), fg='RED',  bg="#F57C00")
           erro_banca.place(relx=0.05, rely=0.25)
           self.janela.after(2000, erro_banca.place_forget) # remove o erro apos 2 segundos
+     # APOSTA COM VALORES DA ODD E APOSTA
+    def aposta(self):
+       #label aposta
+       self.label_aposta = Label(self.frame1, text="APOSTA", font=('arial', 30))
+       self.label_aposta.place(relx=0.04, rely=0.08)
+    def valor_aposta(self):
+       self.label_valor = Label(self.frame1, text="Valor Da Aposta:", font=('arial',25))
+       self.label_valor.place(relx=0.04, rely=0.14)
+       # ENTRADA
+       self.entry_valor = Entry(self.frame1, text="Valor Da Aposta:", font=('arial',18))
+       self.entry_valor.place(relx=0.04, rely=0.19)
+
+
+
+    def valor_odd(self):
+       self.label_odd = Label(self.frame1, text="Valor Da ODD:", font=('arial',25))
+       self.label_odd.place(relx=0.04, rely=0.24)
+        # ENTRADA
+       self.entry_odd = Entry(self.frame1, text="Valor Da ODD:", font=('arial',18))
+       self.entry_odd.place(relx=0.04, rely=0.29)
+    # BOTAO GRENN E RED
+    def green(self):
+       self.botao_green = Button(self.frame1,text="GREEN", font=('arial',30,'bold'))
+       self.botao_green.place(relx=0.04, rely=0.33)
+    def red(self):
+       self.botao_red = Button(self.frame1,text="RED", font=('arial',30,'bold'))
+       self.botao_red.place(relx=0.34, rely=0.33)
+
 
 Applicação()
